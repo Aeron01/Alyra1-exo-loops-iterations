d7cded576ebfe4949035b65474d7931f3e6d6ec1
Aeron01 <one.soulreaper@gmail.com>
2021-02-26 15:38:17 +0100

    2nd exercise done

---
+++ loop2.js
@@ -0,0 +1,10 @@
+// Avec une boucle do-while afficher les éléments du tableau suivant, ligne par ligne:
+
+let tab = ['a', 'b', 'c', 'd', 'e']
+
+let i = 0
+do {
+  console.log(tab[i])
+  i = i + 1
+} while (i < 4) {
+}
