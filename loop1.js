900955ab1b47f7ac92ffdb73f63c66074f8ff268
Aeron01 <one.soulreaper@gmail.com>
2021-02-26 15:51:38 +0100

    I found another solution

--- loop1.js
+++ loop1.js
@@ -6,3 +6,13 @@ while (i <= 9) {
   console.log(tab[i])
   i++
 }
+
+// ou cette solution
+
+console.log(`\t`)
+let tab2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let i2 = 0
+while (i2 < tab2.length) {
+  console.log(tab[i2])
+  i2++
+}
