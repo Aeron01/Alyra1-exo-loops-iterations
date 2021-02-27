14d11afb8ec1b4045f08358c3ab4ac7973c3dea5
Aeron01 <one.soulreaper@gmail.com>
2021-02-27 13:16:32 +0100

    minor update

--- onlyMultipleOf3And7.js
+++ onlyMultipleOf3And7.js
@@ -9,3 +9,22 @@ for (let i = 1; i <= 1000; ++i) {
   }
   console.log(i)
 }
+
+
+// Autre solution plus longue
+console.log(`\n\n\n`)
+
+for (let i2 = 1; i2 <= 1000; ++i2) {
+  if (i2 % 3) {
+    continue
+  }
+  console.log(`Multiple de 3 : ${i2}`)
+}
+console.log(`\n`)
+
+for (let i3 = 1; i3 <= 1000; ++i3) {
+  if (i3 % 7) {
+    continue
+  }
+  console.log(`Multiple de 7 : ${i3}`)
+}
