ca1b1116d31d61a70108ab8fbc388ad842d60bad
Aeron01 <one.soulreaper@gmail.com>
2021-03-05 09:58:19 +0100

    update

--- sumAll3D.js
+++ sumAll3D.js
@@ -29,7 +29,7 @@ console.log(`sum = ${sum}`)*/
 /*let sum = 0       // version que j'ai tapez pendant la correction et retourne 0 ???!!! (raison inconnue)
 for (let i = 0; i < tab.length; ++i) {
   for (let j = 0; j < tab[i].length; ++j) {
-    for (let k = 0; k < tab[i][j].lenght; ++k) {
+    for (let k = 0; k < tab[i][j].lenght; ++k) { // voila l'affreuses erreur de typo !!!! .lenght au lieu de .length !
       sum += tab[i][j][k]
     }
   }
